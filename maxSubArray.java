82b9faab8cbb634167062dd80772d98a447bc727
Mahi-Mani <m.mahishamani@gmail.com>
2025-09-27 23:16:23 -0700

    Implement Kadane's algorithm

---
+++ maxSubArray.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int res = nums[0];
+
+        for(int i=1;i<nums.length; i++) {
+            maxSum = Math.max(maxSum + nums[i], nums[i]);
+            res = Math.max(res, maxSum);
+        }
+        return res;
+    }
+}
